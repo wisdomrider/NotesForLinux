fc00268bda882174326f810401711f0298aa2572
avishek <avishekzone@gmail.com>
2019-07-12 17:43:03 +0545

    autocomplete added

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ pip3 install colorama
 pip3 install requests
 clear
 sudo mv wisdnote /usr/bin/
-complete -W "add edit list delete relogin help search" wisdnote
+sudo echo "complete -W 'add edit list delete relogin help search' wisdnote">>/etc/bash.bashrc
 echo Succesfully Installed !
