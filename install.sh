1c3ab8e6b8a23b001c4ce637951152c6576e3645
avishek <avishekzone@gmail.com>
2019-07-17 20:20:54 +0545

    double entry error fixed

--- install.sh
+++ install.sh
@@ -4,5 +4,9 @@ pip3 install colorama
 pip3 install requests
 clear
 sudo mv wisdnote /usr/bin/
-sudo echo "complete -W 'add edit list delete relogin help search' wisdnote">>/etc/bash.bashrc
+if grep -q "complete -W 'add edit list delete relogin help search' wisdnote" "/etc/bash.bashrc"; then
+  echo 
+else
+    sudo echo "complete -W 'add edit list delete relogin help search' wisdnote">>/etc/bash.bashrc
+fi
 echo Succesfully Installed !
